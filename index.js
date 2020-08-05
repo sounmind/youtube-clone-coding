894cdd2b1745193361860d9a8cbaa43345ac3e44
sounmind <sounmind777@gmail.com>
2020-08-05 14:37:21 +0900

    #2.7 Express Core: Middlewares part Two 하기 전 수정

--- index.js
+++ index.js
@@ -2,34 +2,21 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
-// app.listen(4000); // 포트넘버 4000
-/*  
-localhost:4000 으로 접속하면 `Cannot GET /`이라고 웹페이지에 출력된다.
-일단 서버가 listen 하고 있다는 뜻. 최소한 서버가 있다는 뜻!
-*/
-
-/*
-package.json 파일 내부에 아래와 같이 수정하면 node index.js 명령 대신, npm start 로 실행할 수 있게 된다.
- ...
- ...
- ...
-  "dependencies": {
-    "express": "^4.17.1"
-  },
-  "scripts":{
-    "start": "node index.js" 
-  }
-}
-*/
-
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (requestObject, responseObject) => responseObject.send("Hello from home!")
-
+const handleHome = (requestObject, responseObject) => responseObject.send("Hello from home!");
 
 const handleProfile = (requestObject, responseObject) => responseObject.send("You are on my profile."); // 진짜 웹사이트는 Html css를 send 해야 한다.
 
-app.get("/", handleHome);
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
+app.get("/", handleHome); // 미들웨어를 중간에 넣었다!! 하지만 이 상태에선 구글 크롬으로부터 온 요청을 계속 처리할 지에 대해 권한을 주지 않았다.
+
+app.use(betweenHome); // 모든 웹사이트 사용에 대해서 미들웨어를 넣기 // 순서가 중요하다! // 이 아래에 모든 미들웨어를 둔 다음에 라우트를 처리해야 모든 기록을 받을 수 있다.
+
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
