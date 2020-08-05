4b182188d4b6981ef1d5aee332cece44469de473
sounmind <sounmind777@gmail.com>
2020-08-05 13:51:56 +0900

    JS컴파일러 babel을 이용해서 쉽게 함수를 만들고, 서버를 재실행할 수 있게 됨

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express'); // node modules을 어딘가에서 가져오는 것, 괄호 안에 있는 이름의 폴더를 찾으려고 하고, 못 찾으면 node_modules 안에서 찾는다.
-const { response } = require('express');
-const app = express(); // express application 실행 및 선언
+import express from "express";
+const app = express();
 const PORT = 4000;
 
 // app.listen(4000); // 포트넘버 4000
@@ -23,19 +22,12 @@ package.json 파일 내부에 아래와 같이 수정하면 node index.js 명령 대신, npm start
 }
 */
 
-function handleListening(){
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(requestObject, responseObject){
-  console.log(requestObject);
-  responseObject.send("Hello from home!")
-}
+const handleHome = (requestObject, responseObject) => responseObject.send("Hello from home!")
 
-function handleProfile(requestObject, responseObject){
-  console.log(requestObject);
-  responseObject.send("You are on my profile."); // 진짜 웹사이트는 Html css를 send 해야 한다.
-}
+
+const handleProfile = (requestObject, responseObject) => responseObject.send("You are on my profile."); // 진짜 웹사이트는 Html css를 send 해야 한다.
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
