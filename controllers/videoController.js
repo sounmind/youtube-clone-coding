78508fd509a8a1d2283da89dafea8d65e94d5ee1
sounmind <sounmind777@gmail.com>
2020-09-28 13:15:01 +0900

    ~ #3.1 Connectiong to MongoDB

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,17 +1,26 @@
-import { videos } from "../db";
 import routes from "../routes";
 
 export const home = (req, res) => {
-    res.render("home", { pageTitle: "Home", videos }); // render 함수의 첫번째 인자는 템플릿, 두번째 인자는 템플릿에 추가할 정보가 담긴 객체다.
+    res.render("home", { 
+        pageTitle: "Home", 
+        videos 
+    }); // render 함수의 첫번째 인자는 템플릿, 두번째 인자는 템플릿에 추가할 정보가 담긴 객체다.
 };
 
 export const search = (req, res) => {
-    const {query: { term: searchingBy } } = req; // ES6 이전의 방식: const searchingBy = req.query.term;
-    res.render("search", { pageTitle: "Search", searchingBy: searchingBy, videos }); // 그냥 serachingBy만 입력해줘도 Babel이 같은 의미로 해석해준다.
+    const {query: { term: searchingBy } } = req; 
+    // ES6 이전의 방식: const searchingBy = req.query.term;
+    res.render("search", { 
+        pageTitle: "Search", 
+        searchingBy: searchingBy, 
+        videos 
+    }); // 그냥 serachingBy만 입력해줘도 Babel이 같은 의미로 해석해준다.
 };
 
 export const getUpload = (req, res) => {
-    res.render("upload", { pageTitle: "Upload" });
+    res.render("upload", { 
+        pageTitle: "Upload" 
+    });
 }
 
 export const postUpload = (req, res) => {
--- db.js
+++ db.js
@@ -1,51 +1,18 @@
-// fake db
-export const videos = [
-    {
-      id: 324393,
-      title: "Video awesome",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 1212121,
-      title: "Video super",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 55555,
-      title: "Video nice",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 11111,
-      title: "Video perfect",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    }
-  ];
+import mongoose from "mongoose";
+
+// 해당 경로에 string으로 된 DB를 요청
+mongoose.connect(
+  "mongodb://localhost:27017/we-tube",
+  { // 새로운 버전의 Mongoose의 기능, DB 연결할 때마다 configuration 설정
+    useNewUrlParser: true,
+    userFindAndModify: false
+  }
+); 
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB")
+const handleError = (error) => console.log(`❌ Error on DB connection: ${error}`)
+
+db.once("open", handleOpen); // 한 번 실행
+db.on("error", handleError);
