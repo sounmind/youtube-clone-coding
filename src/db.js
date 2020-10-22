ae25dca807c5a4730aa703fba0f040f1f573fb6c
sounmind <sounmind777@gmail.com>
2020-10-23 03:18:59 +0900

    Modify Passport.js

--- src/db.js
+++ src/db.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // 해당 경로에 string으로 된 DB를 요청
-mongoose.connect(process.env.MONGO_URL, {
-  // 새로운 버전의 Mongoose의 기능, DB 연결할 때마다 configuration 설정
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    // 새로운 버전의 Mongoose의 기능, DB 연결할 때마다 configuration 설정
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
 
 const db = mongoose.connection;
 
--- src/passport.js
+++ src/passport.js
@@ -14,7 +14,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+      callbackURL: process.env.PRODUCTION
+        ? `https://lit-badlands-93629.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`,
     },
     githubLoginCallback
   )
